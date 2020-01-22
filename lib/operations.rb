3bfdd025f2051cf3e247224843a2781e1b5cbc08
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2020-01-22 22:14:25 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+if speed>60? true: false
+  elsif speed<40? true: false 
+elsif !(speed>40 && speed<60)? true:false 
 end
 
 
-
 def not_safe?(speed)
-	
+	if speed>60? true: false
+	  elsif speed<40? ture:false 
+  	elsif !(speed>40 && speed<60) ? true : false 
 end
 	
 
