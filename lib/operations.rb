4b665e71a3f0de734cc358496f0a3cffd42d0537
Abel Kalum <chumbabel@gmail.com>
2020-01-22 23:53:26 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+  if speed > 60 ? true : false
+    return true
+  elsif speed < 40 ? true : false
+    return true
+  else
+    return false
+  end
 end
-
-
-
+  
 def not_safe?(speed)
-	
+  if speed > 60 || speed < 40 ? true : false
+    return true
+  else
+    return false
+  end
 end
-	
-
-
